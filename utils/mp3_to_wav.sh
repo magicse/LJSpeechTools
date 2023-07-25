32ce199675a4f19465311e15be0440fb015c462b
magicse <magicse@users.noreply.github.com>
2023-07-26 02:07:59 +0300

    Update mp3_to_wav.sh

--- utils/mp3_to_wav.sh
+++ utils/mp3_to_wav.sh
@@ -3,6 +3,6 @@ for file in *.mp3 do
         echo "Converting $file"
         # replace .mp3 extension with .wav extension
         new_file=${file%.mp3}.wav
-        ffmpeg -i $file -acodec pcm_s16le -ac 1 -ar 16000 $new_file
+        ffmpeg -i $file -acodec pcm_s16le -ac 1 -ar 22050 $new_file
     done
 done
